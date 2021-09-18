7cb5d33dd7e2662549136ac6757f71b01f24b94c
nanthony <nanthony@uwaterloo.ca>
2021-09-18 02:53:47 -0400

    push

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,14 @@
+
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    type: Phaser.CANVAS,
+    width: 1000,
+    height: 1000,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -13,10 +20,15 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('whiteBall', 'images/ball.png');
+
 }
 
 function create ()
 {
+    balls = this.physics.add.group();
+
+    balls.create(300, 300, 'whiteBall').setScale(0.5).refreshBody().setBounce(1).setCollideWorldBounds(true).setVelocityX(100).setVelocityY(50);
 }
 
 function update ()
